1f7478e41364724bcc25762e9ff8c2a174c13bcd
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2023-10-04 00:32:07 +0200

    first nice prototype with video and footline

---
+++ extract_frames.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ensure the script stops on the first error
+set -e
+
+# Navigate to the videos directory (optional, adjust as per your folder structure)
+cd videos
+
+# Create extracted_frames directory if it doesn't exist
+mkdir -p extracted_frames
+
+# Loop through all .mp4 files in the videos directory
+for video in *.mp4; do
+    # Extract the video filename without the extension
+    video_name="${video%.*}"
+
+    # Extract the first frame and save it to extracted_frames/{video_name}_first_frame.jpg
+    ffmpeg -i "$video" -ss 00:00:00 -vframes 1 "extracted_frames/${video_name}_first_frame.jpg"
+done
